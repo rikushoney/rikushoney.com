8fb29b60cc6532b72746c26451127d9304f609ca
Rikus Honey <contact@rikushoney.com>
2020-10-07 23:54:23 +0200

    [revamp] start with cleanup

--- src/build/sass_build.py
+++
@@ -1,9 +0,0 @@
-import sass
-
-
-class SassBuild:
-    def __init__(self, styles_path):
-        self._styles = styles_path
-
-    def compile_styles(self, target, format="expanded"):
-        sass.compile(dirname=(self._styles, target), output_style=format)
